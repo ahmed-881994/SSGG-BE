1ec3f829622616d5f4dd7dead6810d4d1f7ce53f
Ahmed Safwat <ahmed-881994@hotmail.com>
2023-08-26 19:45:50 +0300

    update

--- Lambda/SSGG-GetMember/lambda_function.py
+++ Lambda/SSGG-GetMember/lambda_function.py
@@ -1,7 +1,31 @@
 import json
+import os
+import pymysql.cursors
+
+
+def connect():
+    try:
+        # connect to database
+        # reading the database parameters from the config object
+        conn = pymysql.connect(
+            host=os.environ.get("host"),
+            database=os.environ.get("database"),
+            user=os.environ.get("username"),
+            password=os.environ.get("password")
+            # cursorclass=pymysql.cursors.DictCursor
+        )
+        return conn
+    except Exception as error:
+        print(error)
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 500,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"message": "Couldn't reach the database"}),
+        }
+
+
 def lambda_handler(event, context):
     # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda 5')
-    }
+    return connect()
+    #return {"statusCode": 200, "body": json.dumps("Hello from Lambda 5")}

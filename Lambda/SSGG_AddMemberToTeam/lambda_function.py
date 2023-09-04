7afbf8df25da6e489d40870de4ff64474c0d75f3
Ahmed Safwat <ahmed-881994@hotmail.com>
2023-09-04 13:43:27 +0300

    add 'AddMemberToTeam' function

---
+++ Lambda/SSGG_AddMemberToTeam/lambda_function.py
@@ -0,0 +1,82 @@
+import json
+import os
+import pymysql.cursors
+
+def connect():
+    try:
+        # Connect to the database
+        cursor = pymysql.cursors.DictCursor
+        conn = pymysql.connect(
+            host=os.environ.get("host"),
+            port=int(os.environ.get("port")),
+            database=os.environ.get("database"),
+            user=os.environ.get("username"),
+            password=os.environ.get("password"),
+            cursorclass=cursor,
+        )
+        response = None
+    except Exception as error:
+        print(error)
+        conn = None
+        response = {
+            "isBase64Encoded": False,
+            "statusCode": 500,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"message": "Couldn't reach the database"}),
+        }
+    return conn, response
+
+def lambda_handler(event, context):
+    conn, response = connect()
+
+    if response is None:
+        with conn.cursor() as cursor:
+            try:
+                body = json.loads(event["body"])
+                memberID=body.get("MemberID")
+                teamID = event['pathParameters']['teamID']
+                cursor.callproc("GetMember", [memberID])
+                records = cursor.fetchone()
+                if records is not None:
+                    if records.get('team_id') == teamID:
+                        response = {
+                            "isBase64Encoded": False,
+                            "statusCode": 400,
+                            "headers": {"Content-Type": "application/json"},
+                            "body": json.dumps({"message": "Member already belongs to team"}),
+                        }
+                    else:
+                        is_leader = 1 if body.get("IsLeader") else 0
+                        from_date= body.get("FromDate")
+                        args = [
+                            memberID,
+                            teamID,
+                            from_date,
+                            is_leader
+                        ]
+                        cursor.callproc("AddMemberToTeam", args)
+                        conn.commit()
+                        response = {
+                            "isBase64Encoded": False,
+                            "statusCode": 201,
+                            "headers": {"Content-Type": "application/json"},
+                            "body": json.dumps(
+                                {"message": "Member added to team"}
+                            ),
+                        }
+                else:
+                    response = {
+                        "isBase64Encoded": False,
+                        "statusCode": 404,
+                        "headers": {"Content-Type": "application/json"},
+                        "body": json.dumps({"message": "Member not found"}),
+                    }
+            except Exception as error:
+                response = {
+                    "isBase64Encoded": False,
+                    "statusCode": 500,
+                    "headers": {"Content-Type": "application/json"},
+                    "body": json.dumps({"message": error.args[1]}),
+                }
+
+    return response

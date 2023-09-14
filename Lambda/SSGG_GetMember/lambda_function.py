e9953e5f7cfffda122886c73695233f49f6c4757
Ahmed Safwat <ahmed-881994@hotmail.com>
2023-09-14 13:29:48 +0300

    handle multiple teams

--- Lambda/SSGG_GetMember/lambda_function.py
+++ Lambda/SSGG_GetMember/lambda_function.py
@@ -2,6 +2,7 @@ import json
 import os
 import pymysql.cursors
 
+
 def connect():
     try:
         # Connect to the database
@@ -25,72 +26,77 @@ def connect():
         }
     return conn, response
 
-def format_record(records):
-    data = {}
-    data['MemberID'] = records['member_id']
-    
-    field_mappings = {
-        'Name': {'EN': 'name_en', 'AR': 'name_ar'},
-        'TeamID': 'team_id',
-        'TeamName': {'EN': 'team_name_en', 'AR': 'team_name_ar'},
-        'PlaceOfBirth': 'place_of_birth',
-        'DateOfBirth': 'date_of_birth',
-        'Address': 'address',
-        'NationalIdNo': 'national_id_no',
-        'ClubIdNo': 'club_id_no',
-        'PassportNo': 'passport_no',
-        'DateJoined': 'date_joined',
-        'MobileNo': 'mobile_number',
-        'HomeContact': 'home_contact',
-        'Email': 'email',
-        'FacebookURL': 'facebook_url',
-        'SchoolName': 'school_name',
-        'EducationType': 'education_type',
-        'FatherName': 'father_name',
-        'FatherContact': 'father_contact',
-        'FatherJob': 'father_job',
-        'MotherName': 'mother_name',
-        'MotherContact': 'mother_contact',
-        'MotherJob': 'mother_job',
-        'GuardianName': 'guardian_name',
-        'GuardianContact': 'guardian_contact',
-        'GuardianRelationship': 'guardian_relationship',
-        'Hobbies': 'hobbies',
-        'HealthIssues': 'health_issues',
-        'Medications': 'medications',
-        'QRCodeURL': 'qr_code_url',
-        'ImageURL': 'image_url',
-        'NationalIdURL': 'national_id_url',
-        'ParentNationalIdURL': 'parent_national_id_url',
-        'ClubIdURL': 'club_id_url',
-        'PassportURL': 'passport_url',
-        'BirthCertificateURL': 'birth_certificate_url',
-        'PhotoConsent': 'photo_consent',
-        'ConditionsConsent': 'conditions_consent'
+
+def format_records(records):
+    formatted_entry = {
+        "MemberID": records[0].get("member_id"),
+        "Name": {
+            "EN": records[0].get("name_en"),
+            "AR": records[0].get("name_ar"),
+        },
+        "Teams": [],
+        "PlaceOfBirth": records[0].get("place_of_birth"),
+        "DateOfBirth": records[0].get("date_of_birth"),
+        "Address": records[0].get("address"),
+        "NationalIdNo": records[0].get("national_id_no"),
+        "ClubIdNo": records[0].get("club_id_no"),
+        "PassportNo": records[0].get("passport_no"),
+        "DateJoined": records[0].get("date_joined"),
+        "MobileNo": records[0].get("mobile_number"),
+        "HomeContact": records[0].get("home_contact"),
+        "Email": records[0].get("email"),
+        "FacebookURL": records[0].get("facebook_url"),
+        "SchoolName": records[0].get("school_name"),
+        "EducationType": records[0].get("education_type"),
+        "FatherName": records[0].get("father_name"),
+        "FatherContact": records[0].get("father_contact"),
+        "FatherJob": records[0].get("father_job"),
+        "MotherName": records[0].get("mother_name"),
+        "MotherContact": records[0].get("mother_contact"),
+        "MotherJob": records[0].get("mother_job"),
+        "GuardianName": records[0].get("guardian_name"),
+        "GuardianContact": records[0].get("guardian_contact"),
+        "GuardianRelationship": records[0].get("guardian_relationship"),
+        "Hobbies": records[0].get("hobbies"),
+        "HealthIssues": records[0].get("health_issues"),
+        "Medications": records[0].get("medications"),
+        "QRCodeURL": records[0].get("qr_code_url"),
+        "ImageURL": records[0].get("image_url"),
+        "NationalIdURL": records[0].get("national_id_url"),
+        "ParentNationalIdURL": records[0].get("parent_national_id_url"),
+        "ClubIdURL": records[0].get("club_id_url"),
+        "PassportURL": records[0].get("passport_url"),
+        "BirthCertificateURL": records[0].get("birth_certificate_url"),
+        "PhotoConsent": records[0].get("photo_consent"),
+        "ConditionsConsent": records[0].get("conditions_consent"),
     }
-    
-    for key, value in field_mappings.items():
-        if isinstance(value, str):
-            data[key] = records[value]
-        else:
-            data[key] = {
-                lang: records[field] for lang, field in value.items()
-            }
-    
-    return data
+
+    for record in records:
+        team_entry = {
+            "TeamID": record.get("team_id"),
+            "IsTeamLeader": record.get("is_leader"),
+            "TeamName": {
+                "EN": record.get("team_name_en"),
+                "AR": record.get("team_name_ar"),
+            },
+        }
+        formatted_entry["Teams"].append(team_entry)
+
+    return formatted_entry
+
 
 def lambda_handler(event, context):
     conn, response = connect()
-    
+
     if response is None:
         with conn.cursor() as cursor:
             try:
-                memberID = event['pathParameters'].get('memberID')
+                memberID = event["pathParameters"].get("memberID")
                 cursor.callproc("GetMember", [memberID])
                 records = cursor.fetchone()
-                
+
                 if records is not None:
-                    data = format_record(records)
+                    data = format_records(records)
                     response = {
                         "isBase64Encoded": False,
                         "statusCode": 200,
@@ -111,5 +117,5 @@ def lambda_handler(event, context):
                     "headers": {"Content-Type": "application/json"},
                     "body": json.dumps({"message": error.args[1]}),
                 }
-    
+
     return response

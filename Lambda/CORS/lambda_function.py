28ab20a3fba0c2a669b0e6cd7c86b877500bc485
Ahmed Safwat <ahmed-881994@hotmail.com>
2024-05-08 19:44:33 +0300

    adjust cors response

--- Lambda/CORS/lambda_function.py
+++ Lambda/CORS/lambda_function.py
@@ -4,7 +4,10 @@ def lambda_handler(event, context):
     response = {
         "isBase64Encoded": False,
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"}
+        "headers": {"Content-Type": "application/json",
+                    'Access-Control-Allow-Headers': '*',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*'}
     }
 
     return response

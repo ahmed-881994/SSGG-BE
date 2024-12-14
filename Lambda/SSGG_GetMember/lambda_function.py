b90378b75391e43dc8412177f91fac01c593d509
Ahmed Safwat <ahmed-881994@hotmail.com>
2024-12-15 00:26:55 +0200

    enable logging for Get_Member (#1)

--- Lambda/SSGG_GetMember/lambda_function.py
+++ Lambda/SSGG_GetMember/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 import os
 import pymysql.cursors
-
+import logging
 
 def connect():
     try:
@@ -89,6 +89,9 @@ def format_records(records):
 
 
 def lambda_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.info(event)
     conn, response = connect()
 
     if response is None:

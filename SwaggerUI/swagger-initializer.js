23e10f833ba7d02e48356b1bf0d9b7052a202fc3
Ahmed Safwat <ahmed-881994@hotmail.com>
2025-03-14 15:42:58 +0200

    Update Swagger UI initializer to use JSON format for API definition

--- SwaggerUI/swagger-initializer.js
+++ SwaggerUI/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     //url: "https://petstore.swagger.io/v2/swagger.json",
-    url: "https://ssgg-swagger.s3.eu-north-1.amazonaws.com/SSGG.yaml",
+    url: "https://ssgg-swagger.s3.eu-north-1.amazonaws.com/SSGG.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
